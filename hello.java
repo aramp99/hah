091d5d2499ee4cbfe0fea135ccf0f816acbc49db
aramp99 <41547644+aramp99@users.noreply.github.com>
2018-07-23 21:15:50 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,31 @@
+public class hello	{
+	final static String ana="RAAAM IS COOLER THAN JYO";
+		public static void main(String[] args)	{
+		/*boolean no=true;
+		boolean yes=false;
+		if(yes && no)	
+			System.out.println("SMILEEEEEEEEEEEEE");
+		else
+			System.out.println("NOOOOOOOOO");*/
+		//float ab=2.305f;
+		//double a=2.545;
+		//Float.toString(ab);
+		//String a=Float.toString(ab);
+		//float cd=Float.parseFloat("RAM");
+		//String[] ab = new String[3];
+		//ab[0]= null; ab[1]=" ";
+		//System.out.println(ab[1] + ab[0]);
+		//System.out.println(a);
+		//System.out.println(ab + a);
+		//System.out.println(ab + " " + a);
+		//if(args[1].equals("3"))
+		//	System.out.println(args[0] + args[1]);
+		//System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+		String ana="   RAAAMAMAMAMAMA   ";
+		//cool=cool.replace('A',' ');
+		//cool=cool.trim();
+		//System.out.println(Math.round(ra)+" "+Math.floor(ra)+" "+Math.ceil(ra));
+		System.out.println(ana +" "+hello.ana);
+		dumbjyo.hah(); coolram.haah();
+	}
+}
